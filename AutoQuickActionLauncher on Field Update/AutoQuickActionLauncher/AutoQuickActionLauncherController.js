efc25db26d61aa22a787bee641b2aea7cbe06331
ForcePanda <forcepanda07@gmail.com>
2020-06-28 23:40:02 +0200

    Updated folder visibility

--- AutoQuickActionLauncher on Field Update/AutoQuickActionLauncher/AutoQuickActionLauncherController.js
+++
@@ -1,8 +0,0 @@
-({
-    init : function(cmp, event, helper) {
-        helper.setAttributes(cmp);        
-        
-        //Validating User Input Parameters
-        helper.validateUserInputs(cmp);
-    }
-})
